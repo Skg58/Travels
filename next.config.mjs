bb7128bbb108a88974f562f1e2522317dd74e99e
Suraj Kumar <kumarsuraj841504@gmail.com>
2025-06-28 18:14:37 +0530

    some update about favicon

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,18 @@
 const nextConfig = {devIndicators: false ,  reactStrictMode: false,eslint: {
     ignoreDuringBuilds: true,
   },images: {
-    remotePatterns: [new URL('https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80')],
-     domains: [
-      'avatars.githubusercontent.com', 
-      'lh3.googleusercontent.com'      
-    ],
+    remotePatterns: [new URL('https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80'),{
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },],
+    //  domains: [
+    //   'avatars.githubusercontent.com', 
+    //   'lh3.googleusercontent.com'      
+    // ],
   },};
 
 export default nextConfig;
